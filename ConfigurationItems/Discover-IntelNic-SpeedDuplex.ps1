71dfe6139062846ded01f742d94865d057506fa4
Jeroen-J-Bakker <51163393+Jeroen-J-Bakker@users.noreply.github.com>
2024-04-16 23:21:06 +0200

    Add files via upload

---
+++ ConfigurationItems/Discover-IntelNic-SpeedDuplex.ps1
@@ -0,0 +1,78 @@
+﻿<#
+Discover-IntelNic-SpeedDuplex.ps1
+
+Discover if network adapter speed is configured at 100Mbps full duplex
+Change the $Duplexvalue variable on lines 64 and 65 for other speed/ duplex settings
+
+Version: 2.0
+Author: Jeroen Bakker
+
+To be used as an discovery script within a SCCM Configuration Item (with application settings)
+Output:
+* 100Mbps full duplex enabled --> "Enabled"
+* Other network speed duplex setting --> "Disabled"
+
+Related Link speed and Duplex mode registry values (tested for e1d adapter type, may differ for e1c)
+0 = Auto Negotiate
+1 = 10 Mbps Half Duplex
+2 = 10 Mbps Full Duplex
+3 = 100 Mbps Half Duplex
+4 = 100 Mbps Full Duplex
+6 = 1.0 Gbps Full Duplex
+
+
+Version history
+---------------
+Version 1.0: 03-02-2017: Initial production version
+Version 2.0: 16-04-2024: Generalized script for reuse and distribution
+#>
+
+# Set registry path to Network Adapter class
+$RegPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}"
+
+#Create a hash table with all supported LAN network adapters and their series
+#e1d = Intel I217 /218 /219 series
+#e1c = Intel 82579 series
+
+$Adapters = @{
+    "Intel(R) Ethernet Connection I217-LM"="e1d";
+    "Intel(R) Ethernet Connection I217-V"="e1d";
+    "Intel(R) Ethernet Connection I218-LM"="e1d";
+    "Intel(R) Ethernet Connection I218-V"="e1d";
+    "Intel(R) Ethernet Connection (2) I218-LM"="e1d";
+    "Intel(R) Ethernet Connection (2) I218-V"="e1d";
+    "Intel(R) Ethernet Connection (3) I218-LM"="e1d";
+    "Intel(R) Ethernet Connection (3) I218-V"="e1d";
+    "Intel(R) Ethernet Connection I219-LM"="e1d";
+    "Intel(R) Ethernet Connection I219-V"="e1d";
+    "Intel(R) Ethernet Connection (2) I219-LM"="e1d";
+    "Intel(R) Ethernet Connection (2) I219-V"="e1d";
+    "Intel(R) 82579LM Gigabit Network Connection"="e1c";
+    "Intel(R) 82579V Gigabit Network Connection"="e1c"
+}
+
+
+# Walk through all sub-keys to find the LAN network adapter
+Get-ChildItem -Path $RegPath -Exclude "Properties","Configuration"|ForEach {
+
+    # Verify if the adpater is listed as a supported NIC
+    If ($Adapters.ContainsKey((Get-ItemProperty -Path Registry::$_ -name DriverDesc).DriverDesc)){
+
+        # Set registry value name and value data for 100Mbps full duplex based on adapter type
+        switch ($Adapters.get_item((Get-ItemProperty -Path Registry::$_ -name DriverDesc).DriverDesc))
+        {
+        e1c {$DuplexName = "*SpeedDuplex"; $DuplexValue = 4}
+        e1d {$DuplexName = "*SpeedDuplex"; $DuplexValue = 4}
+        }
+        
+        # Test if the network adapter is configured at 100 Mbps full duplex
+        If ((Get-ItemProperty -Path Registry::$_ -name $DuplexName).$DuplexName -ne $DuplexValue){
+            # 100 Mbps full duplex is not enabled
+            Write-Host "Disabled"
+        }
+        Else {
+            # 100 Mbps full duplex is enabled
+            Write-Host "Enabled"
+        }
+    }
+}
---
+++ ConfigurationItems/Remediate-IntelNic-LegacySwitchMode.ps1
@@ -0,0 +1,73 @@
+﻿<#
+Remediate-IntelNic-LegacySwitchMode.ps1
+
+Enable legacy switch mode if required
+
+Version: 2.0
+Author: Jeroen Bakker
+
+To be used as remediation script within a SCCM Configuration Item (with application settings)
+
+Related registry values:
+1 = Legacy switch mode disabled (default)
+2 = Legacy switch mode enabled
+
+Version history
+---------------
+Version 1.0: 03-02-2017: Initial production version
+Version 2.0: 16-04-2024: Generalized script for reuse and distribution
+
+#>
+
+# Set registry path to Network Adapter class
+$RegPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}"
+
+#Create a hash table with all supported LAN network adapters and their series
+#e1d = Intel I217 /218 /219 series
+#e1c = Intel 82579 series
+
+$Adapters = @{
+    "Intel(R) Ethernet Connection I217-LM"="e1d";
+    "Intel(R) Ethernet Connection I217-V"="e1d";
+    "Intel(R) Ethernet Connection I218-LM"="e1d";
+    "Intel(R) Ethernet Connection I218-V"="e1d";
+    "Intel(R) Ethernet Connection (2) I218-LM"="e1d";
+    "Intel(R) Ethernet Connection (2) I218-V"="e1d";
+    "Intel(R) Ethernet Connection (3) I218-LM"="e1d";
+    "Intel(R) Ethernet Connection (3) I218-V"="e1d";
+    "Intel(R) Ethernet Connection I219-LM"="e1d";
+    "Intel(R) Ethernet Connection I219-V"="e1d";
+    "Intel(R) Ethernet Connection (2) I219-LM"="e1d";
+    "Intel(R) Ethernet Connection (2) I219-V"="e1d";
+    "Intel(R) 82579LM Gigabit Network Connection"="e1c";
+    "Intel(R) 82579V Gigabit Network Connection"="e1c"
+}
+
+
+# Walk through all sub-keys to find the LAN network adapter
+Get-ChildItem -Path $RegPath -Exclude "Properties","Configuration"|ForEach {
+
+    # Verify if the adpater is listed as a supported NIC
+    If ($Adapters.ContainsKey((Get-ItemProperty -Path Registry::$_ -name DriverDesc).DriverDesc)){
+
+        
+        # Set registry value name and value data for legacy switch mode based on adapter type
+        switch ($Adapters.get_item((Get-ItemProperty -Path Registry::$_ -name DriverDesc).DriverDesc))
+        {
+        e1c {<# Legacy switch mode option not available#>}
+        e1d {$SwitchModeName = "LinkNegotiationProcess"; $SwitchModeValue = 2}
+        }
+        
+        # Test if the network adapter requires legacy switch mode configuration
+        If ($SwitchModeName){
+            
+            # Test if the legacy switch mode is enabled
+            If ((Get-ItemProperty -Path Registry::$_ -name $SwitchModeName).$SwitchModeName -ne $SwitchModeValue){
+                
+                # Enable legacy switch mode
+                Set-ItemProperty -Path Registry::$_ -name $SwitchModeName -Value $SwitchModeValue
+                
+            }
+         }
+    }
+}
